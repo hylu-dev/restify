d249b4fad4268a54a2d5d136af133f50c6942aa0
Shaelynn <80Currans@gmail.com>
2022-03-04 15:38:19 -0500

    Created User model
    Created the custom User model that inherits from AbstractUser to add avatar and phone_number attributes on top of the standard first_name, last_name, email, password, etc. Included an open-source project in requirements.txt that creates a PhoneField on top of the built-in model fields for checking proper input and preserving the common phone number display

--- P2/accounts/models.py
+++ P2/accounts/models.py
@@ -1,8 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Imported from https://pypi.org/project/django-phone-field/
+# Only works with NA phone numbers; another version works with international if needed
+from phone_field import PhoneField
+
 # from django.db.models import CASCADE
 # from django.contrib.contenttypes.fields import GenericForeignKey
 # from django.contrib.contenttypes.models import ContentType
 
+
+# Omitted foreign keys in the case of creating relationship models
+class User(AbstractUser):
+    avatar = models.ImageField(upload_to='images', help_text='Account profile icon')
+    phone_number = PhoneField(help_text='Account phone number')
+    # owned_restaurant = models.ForeignKey(to=Restaurant, on_delete=CASCADE, null=True)
 
 # Omitted foreign keys in the case of creating relationship models
 class Notification(models.Model):
