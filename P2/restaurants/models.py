97b058eb7290c6cbfc3e02a5c063534ce39bc2e4
Shaelynn <80Currans@gmail.com>
2022-03-03 23:08:31 -0500

    Created FoodItem model
    Created the FoodItem model with the attributes name, description, price, and potential commented out foreign key attribute dependent on the future db design

--- P2/restaurants/models.py
+++ P2/restaurants/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+# from django.db.models import CASCADE
 
 # Create your models here.
+
+# Omitted foreign keys in the case of creating relationship models
+class FoodItem(models.Model):
+    # restaurant = models.ForeignKey(to=Restaurant, on_delete=CASCADE, null=True)
+
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    price = models.FloatField(min=0.0)
