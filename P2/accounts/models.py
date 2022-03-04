f7c19a0be628d5a1a6c27314c6d31ca06a2a9d56
Shaelynn <80Currans@gmail.com>
2022-03-03 22:59:28 -0500

    Created the Notification model
    Created the Notification model with attributes timestamp, body, and type and commented out foreign key attributes dependent on the future db design

--- P2/accounts/models.py
+++ P2/accounts/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+# from django.db.models import CASCADE
+# from django.contrib.contenttypes.fields import GenericForeignKey
+# from django.contrib.contenttypes.models import ContentType
 
-# Create your models here.
+
+# Omitted foreign keys in the case of creating relationship models
+class Notification(models.Model):
+    # user = models.ForeignKey(to=User, on_delete=CASCADE, null=True)
+
+    """
+    GenericForeignKey to link to any of the models, since both a Restaurant or User can be a poster.
+
+    Relating the GenericForeignKey to another model instance works like below:
+
+    art = Article.objects.get(id=1)
+    c = Comment(content_object=art, comm='asdf')
+    c.save()
+    """
+    # content_type =   models.ForeignKey(ContentType)
+	# object_id = models.PositiveIntegerField()
+	# poster = GenericForeignKey('content_type', 'object_id')
+
+    timestamp = models.DateTimeField()
+    body = models.CharField(max_length=200)
+    type = models.CharField(max_length=200)
