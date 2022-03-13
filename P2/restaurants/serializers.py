bd7e4c2ba0091bc6bdd78442c3154aa17d625c00
Shaelynn <80Currans@gmail.com>
2022-03-13 17:54:41 -0400

    Updated the restaurant serializer for UpdateRestaurantView to prevent editing the owner
    Set the read_only attribute to True for the restaurant serializer to prevent users from trying to change the owner of the restaurant

--- P2/restaurants/serializers.py
+++ P2/restaurants/serializers.py
@@ -5,7 +5,7 @@ from restaurants.models import Restaurant
 class RestaurantSerializer(serializers.ModelSerializer):
     # Custom serializer field to show owner name instead of id
     # Two queries, since we're checking the User model as well
-    owner = serializers.CharField(source='owner.get_full_name')
+    owner = serializers.CharField(source='owner.get_full_name', read_only=True)
 
     class Meta:
         model = Restaurant
