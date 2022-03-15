176aea0a79698e39d6971908dd4a4452d194befd
Shaelynn <80Currans@gmail.com>
2022-03-15 02:15:37 -0400

    Ensure users cannot like their own posts and restaurants in LikeRestaurantView and LikePostView
    Revised LikeRestaurantView and LikePostView to ensure that the user trying to like the entity is not the owner of it. If they are the owner, raises a BadRequest exception that returns a 400 Bad Request status. Also fixed a bug in LikeRestaurantView with LikedRestaurant not referring to a Restaurant object.

--- P2/restaurants/views/like.py
+++ P2/restaurants/views/like.py
@@ -1,6 +1,6 @@
 from rest_framework.generics import get_object_or_404, UpdateAPIView
 from rest_framework.permissions import IsAuthenticated
-from django.core.exceptions import ObjectDoesNotExist
+from django.core.exceptions import ObjectDoesNotExist, BadRequest
 
 from restaurants.models import Restaurant, LikedRestaurant, LikedPost, Post
 from accounts.models import Notification
@@ -17,34 +17,39 @@ class LikeRestaurantView(UpdateAPIView):
 
     def get_object(self):
         restaurant = get_object_or_404(Restaurant, id=self.kwargs['id'])
-        """
-        Check that the LikedRestaurant entity exists:
-        
-        If it does exist, add the current user to the users_who_like attribute to track who
-        liked the restaurant.
+            
+        # Ensure that a restaurant owner cannot like their own restaurant to inflate popularity
+        if restaurant.owner.id == self.request.user.id:
+            raise BadRequest
+        else:
+            """
+            Check that the LikedRestaurant entity exists:
+            
+            If it does exist, add the current user to the users_who_like attribute to track who
+            liked the restaurant.
 
-        If it does not exist, create a new LikedRestaurant instance and add the current user to it.
-        """
-        try:
-            likedRes = LikedRestaurant.objects.get(restaurant=restaurant.id)
-            likedRes.users_who_like.add(self.request.user)
-            likedRes.save()
-        except ObjectDoesNotExist:
-            likedRes = LikedRestaurant.objects.create(restaurant=restaurant.id)
-            likedRes.users_who_like.add(self.request.user)
-            likedRes.save()
+            If it does not exist, create a new LikedRestaurant instance and add the current user to it.
+            """
+            try:
+                likedRes = LikedRestaurant.objects.get(restaurant=restaurant)
+                likedRes.users_who_like.add(self.request.user)
+                likedRes.save()
+            except ObjectDoesNotExist:
+                likedRes = LikedRestaurant.objects.create(restaurant=restaurant)
+                likedRes.users_who_like.add(self.request.user)
+                likedRes.save()
 
-        # Create a new Notification object to notify the owner of the restaurant that it was liked
-        notification = Notification.objects.create(
-            source=self.request.user,
+            # Create a new Notification object to notify the owner of the restaurant that it was liked
+            notification = Notification.objects.create(
+                source=self.request.user,
 
-            target=restaurant,
+                target=restaurant,
 
-            body=" has liked your ",
-            type='Like',
-        )
-        notification.users.add(restaurant.owner)
-        notification.save()
+                body=" has liked your ",
+                type='Like',
+            )
+            notification.users.add(restaurant.owner)
+            notification.save()
 
         return restaurant
 
@@ -58,33 +63,38 @@ class LikePostView(UpdateAPIView):
 
     def get_object(self):
         post = get_object_or_404(Post, id=self.kwargs['id'])
-        """
-        Check that the LikedPost entity exists:
 
-        If it does exist, add the current user to the users_who_like attribute to track who
-        liked the post.
+        # Ensure that a user cannot like their own post to inflate popularity
+        if post.user.id == self.request.user.id:
+            raise BadRequest
+        else:
+            """
+            Check that the LikedPost entity exists:
 
-        If it does not exist, create a new LikedPost instance and add the current user to it.
-        """
-        try:
-            likedPost = LikedPost.objects.get(post=post)
-            likedPost.users_who_like.add(self.request.user)
-            likedPost.save()
-        except ObjectDoesNotExist:
-            likedPost = LikedPost.objects.create(post=post)
-            likedPost.users_who_like.add(self.request.user)
-            likedPost.save()
-        
-        # Create a new Notification object to notify the owner of the post that it was liked
-        notification = Notification.objects.create(
-            source=self.request.user,
+            If it does exist, add the current user to the users_who_like attribute to track who
+            liked the post.
 
-            target=post,
+            If it does not exist, create a new LikedPost instance and add the current user to it.
+            """
+            try:
+                likedPost = LikedPost.objects.get(post=post)
+                likedPost.users_who_like.add(self.request.user)
+                likedPost.save()
+            except ObjectDoesNotExist:
+                likedPost = LikedPost.objects.create(post=post)
+                likedPost.users_who_like.add(self.request.user)
+                likedPost.save()
+            
+            # Create a new Notification object to notify the owner of the post that it was liked
+            notification = Notification.objects.create(
+                source=self.request.user,
 
-            body=" has liked your ",
-            type='Like',
-        )
-        notification.users.add(post.user)
-        notification.save()
+                target=post,
+
+                body=" has liked your ",
+                type='Like',
+            )
+            notification.users.add(post.user)
+            notification.save()
 
         return post
