b4e4ba77bbe7416ac386574e6f21479e266fee2b
Shaelynn <80Currans@gmail.com>
2022-03-14 21:03:47 -0400

    Corrected UpdateFoodItemView to ensure that the user is the restaurant owner
    Updated FoodItemView such that the user must be the owner of that restaurant of the FoodItem in order to edit it. Also corrected the endpoint such that if the user is not an owner of a restaurant, the server will not crash or error out

--- P2/restaurants/views/updatemenu.py
+++ P2/restaurants/views/updatemenu.py
@@ -1,26 +1,18 @@
-from django.contrib.auth import authenticate, login
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
-from django.db import IntegrityError
-from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseRedirect
-from django.shortcuts import render
-from django.template.response import TemplateResponse
-from django.urls import reverse
-from django.views import View
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.generics import get_object_or_404, RetrieveAPIView, UpdateAPIView
+from django.http import Http404
+from rest_framework.generics import get_object_or_404, UpdateAPIView
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from rest_framework.views import APIView
 from django.core.exceptions import ObjectDoesNotExist
 
-from restaurants.models import Restaurant, LikedRestaurant, LikedPost, Post, FoodItem
+from restaurants.models import FoodItem
 
-from restaurants.serializers.restaurant import FoodItemSerializer
+from restaurants.serializers import FoodItemSerializer
 
 class UpdateFoodItemView(UpdateAPIView):
     serializer_class = FoodItemSerializer
     permission_classes = [IsAuthenticated]
 
     def get_object(self):
-        return get_object_or_404(FoodItem, restaurant=self.request.user.owner, id=self.kwargs['id'])
+        try:
+            return get_object_or_404(FoodItem, restaurant=self.request.user.owner, id=self.kwargs['id'])
+        except ObjectDoesNotExist:
+            raise Http404
