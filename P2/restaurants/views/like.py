544b1da5a249f989952120c49f63363022e7895b
Shaelynn <80Currans@gmail.com>
2022-03-15 01:55:16 -0400

    Updated LikePostView and LikeRestaurantView to handle the creation of notifications
    Revised LikePostView and LikeRestaurantView to create a notification object with data on the user that liked the post/restaurant and the post/restaurant that was liked. Also tracks who should be notified in different case scenarios

--- P2/restaurants/views/like.py
+++ P2/restaurants/views/like.py
@@ -3,6 +3,7 @@ from rest_framework.permissions import IsAuthenticated
 from django.core.exceptions import ObjectDoesNotExist
 
 from restaurants.models import Restaurant, LikedRestaurant, LikedPost, Post
+from accounts.models import Notification
 
 from restaurants.serializers import LikedRestaurantSerializer, LikedPostSerializer;
 
@@ -16,6 +17,14 @@ class LikeRestaurantView(UpdateAPIView):
 
     def get_object(self):
         restaurant = get_object_or_404(Restaurant, id=self.kwargs['id'])
+        """
+        Check that the LikedRestaurant entity exists:
+        
+        If it does exist, add the current user to the users_who_like attribute to track who
+        liked the restaurant.
+
+        If it does not exist, create a new LikedRestaurant instance and add the current user to it.
+        """
         try:
             likedRes = LikedRestaurant.objects.get(restaurant=restaurant.id)
             likedRes.users_who_like.add(self.request.user)
@@ -24,6 +33,19 @@ class LikeRestaurantView(UpdateAPIView):
             likedRes = LikedRestaurant.objects.create(restaurant=restaurant.id)
             likedRes.users_who_like.add(self.request.user)
             likedRes.save()
+
+        # Create a new Notification object to notify the owner of the restaurant that it was liked
+        notification = Notification.objects.create(
+            source=self.request.user,
+
+            target=restaurant,
+
+            body=" has liked your ",
+            type='Like',
+        )
+        notification.users.add(restaurant.owner)
+        notification.save()
+
         return restaurant
 
 class LikePostView(UpdateAPIView):
@@ -36,6 +58,14 @@ class LikePostView(UpdateAPIView):
 
     def get_object(self):
         post = get_object_or_404(Post, id=self.kwargs['id'])
+        """
+        Check that the LikedPost entity exists:
+
+        If it does exist, add the current user to the users_who_like attribute to track who
+        liked the post.
+
+        If it does not exist, create a new LikedPost instance and add the current user to it.
+        """
         try:
             likedPost = LikedPost.objects.get(post=post)
             likedPost.users_who_like.add(self.request.user)
@@ -44,4 +74,17 @@ class LikePostView(UpdateAPIView):
             likedPost = LikedPost.objects.create(post=post)
             likedPost.users_who_like.add(self.request.user)
             likedPost.save()
+        
+        # Create a new Notification object to notify the owner of the post that it was liked
+        notification = Notification.objects.create(
+            source=self.request.user,
+
+            target=post,
+
+            body=" has liked your ",
+            type='Like',
+        )
+        notification.users.add(post.user)
+        notification.save()
+
         return post
