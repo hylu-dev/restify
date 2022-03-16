6653e3523d3687a4cd175eb169377d7e8c0dee6b
Riley Hannigan <riley.hannigan@hotmail.com>
2022-03-15 21:41:30 -0400

    edit parameters for docs

--- P2/accounts/views/comment.py
+++ P2/accounts/views/comment.py
@@ -1,27 +1,12 @@
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from accounts.serializers import CommentSerializer;
-from rest_framework.filters import BaseFilterBackend
-import coreapi
-import coreschema
-
-class CustomFilter(BaseFilterBackend):
-    def get_schema_fields(self, view):
-        fields = [
-            coreapi.Field(
-                name="id",
-                schema=coreschema.String(description='the id of the restaurant that owns the post this comment belong to'),
-                required=True,
-                location='path')
-        ]
-        return fields
-
-    def filter_queryset(self, request, queryset, view):
-        return queryset
 
 class CommentView(CreateAPIView):
     """
     Adds a comment to restruant with id of id
+    To create please provide:
+    - **id**: the id of the restaurant being commented on
     """ 
     filter_backends = (CustomFilter,)
     serializer_class = CommentSerializer
--- P2/accounts/views/unfollow.py
+++ P2/accounts/views/unfollow.py
@@ -2,27 +2,12 @@ from rest_framework.generics import get_object_or_404, UpdateAPIView
 from rest_framework.permissions import IsAuthenticated
 from restaurants.models import Restaurant
 from accounts.serializers import UnfollowedRestaurantSerializer
-from rest_framework.filters import BaseFilterBackend
-import coreapi
-import coreschema
-
-class CustomFilter(BaseFilterBackend):
-    def get_schema_fields(self, view):
-        fields = [
-            coreapi.Field(
-                name="id",
-                schema=coreschema.String(description='the id of the restaurant that the user wants to follow'),
-                required=True,
-                location='path')
-        ]
-        return fields
-
-    def filter_queryset(self, request, queryset, view):
-        return queryset
 
 class UnfollowedRestaurantView(UpdateAPIView):
     """
     Removes restaurant from user's follower list, removes user from restaurant's following list
+    To create please provide:
+    - **id**: the id of the restaurant that the user wants to unfollow
     """
     serializer_class = UnfollowedRestaurantSerializer
     permission_classes = [IsAuthenticated]
