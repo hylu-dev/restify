3ae3fa9d364e5127350a839264a7493831dab276
Shaelynn <80Currans@gmail.com>
2022-04-15 16:23:02 -0400

    Implemented basic HTML component for menu
    Transferred the original HTML for the menu into React and created a smaller component for each menu item to be added

---
+++ P3/restify-front/src/components/Common/menu-item/index.js
@@ -0,0 +1,24 @@
+const MenuItem = ({ name, price, description }) => {
+    return <div>
+        <div class="columns" style={{marginLeft: "5%", marginRight: "5%"}}>
+            <div class="column is-narrow">
+                <h3 class="title is-4 has-text-grey-lighter">{ name }</h3>
+            </div>
+
+            <div class="column is-paddingless">
+                <div style={{borderBottom: "1px dashed #b2b2b2", paddingTop: "3%"}}>
+                </div>
+            </div>
+
+            <div class="column is-narrow has-pb-0">
+                <h3 class="title is-4 has-text-grey-lighter">${ price }</h3>
+            </div>
+        </div>
+
+        <div class="columns" style={{marginLeft: "10%", marginRight: "5%"}}>
+            <p>{ description }</p>
+        </div>
+    </div>
+}
+
+export default MenuItem;
--- P3/restify-front/src/pages/restaurant/menu/index.js
+++ P3/restify-front/src/pages/restaurant/menu/index.js
@@ -1,8 +1,35 @@
 //import React, {useEffect, useState} from 'react';
+import MenuItem from "../../../components/Common/menu-item"
 
 const Menu = () => {
     return <>
-        <span>Menu</span>
+        <div class="section" id="Menu-list" style={{backgroundColor: "white"}}>
+            <div class="container">
+                <nav class="level">
+                    <div class="level-left">
+                    </div>
+                
+                    <div class="level-right">
+                        <p class="level-item">
+                            <a href="restaurant-profile-menu-form.html" class="button is-primary" style={{width: "100px"}}>
+                                <span class="icon-text">
+                                    <span class="icon">
+                                        <i class="fas fa-edit"></i>
+                                    </span>
+                                    <span>Edit</span>
+                                </span>
+                            </a>
+                        </p>
+                    </div>
+                </nav>
+            
+                <div class="box p-auto mx-6 has-shadow" style={{backgroundColor: "rgb(214, 159, 135)"}}>
+                    <div class="container box has-text-grey-lighter" style={{backgroundColor: "rgb(59, 59, 63)"}}>
+                        <MenuItem name="Fresh Garden Salad" price="4" description="Better call Saul-ad!"/>
+                    </div>
+                </div>
+            </div>
+        </div>
     </>
 }
 
