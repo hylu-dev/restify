a86f2833be80b71c8d6f04746f85c29e66fa419c
Riley Hannigan <riley.hannigan@hotmail.com>
2022-04-17 04:28:08 -0400

    undo names

--- P3/restify-front/src/components/PostBox/index.js
+++ P3/restify-front/src/components/PostBox/index.js
@@ -3,41 +3,33 @@ import Button from "../../components/Common/button";
 
 const PostBox = ({ username, restaurant, icon, timestamp, text }) => {
     return <>
-        <section className="section">
-            <div className="container">
-                <div className="columns is-centered">
-                    <div className="column is-10">
-                        <div className="box blog-post">
-                            <article className="media">
-                                <div className="media-left">
-                                    <figure className="image is-64x64">
-                                        <img src={icon} alt="Image"></img>
-                                    </figure>
-                                </div>
-                                <div className="media-content">
-                                    <div className="content">
-                                        <p>
-                                            <strong>{username}</strong> <a href="#"><small>{restaurant}</small></a> <small>{timestamp}</small>
-                                            <br></br>
-                                            {text}
-                                        </p>
-                                    </div>
-                                </div>
-                            </article>
-                            <nav className="level is-mobile">
-                                <div className="level-left"></div>
-                                <div className="level-right">
-                                    <div className="heart reply">
-                                        <LikeButton
-                                        />
-                                    </div>
-                                </div>
-                            </nav>
-                        </div>
+        <div className="box blog-post">
+            <article className="media">
+                <div className="media-left">
+                    <figure className="image is-64x64">
+                        <img src={icon} alt="Image"></img>
+                    </figure>
+                </div>
+                <div className="media-content">
+                    <div className="content">
+                        <p>
+                            <strong>{username}</strong> <a href="#"><small>{restaurant}</small></a> <small>{timestamp}</small>
+                            <br></br>
+                            {text}
+                        </p>
                     </div>
                 </div>
-            </div>
-        </section>
+            </article>
+            <nav className="level is-mobile">
+                <div className="level-left"></div>
+                <div className="level-right">
+                    <div className="heart reply">
+                        <LikeButton
+                        />
+                    </div>
+                </div>
+            </nav>
+        </div>
     </>
 
 }
