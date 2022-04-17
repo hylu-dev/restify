581ab229ed647b366769f8e3f1b8a1160ce11c94
luroy <roylu99@gmail.com>
2022-04-17 03:14:07 -0400

    fixes

--- P3/restify-front/src/pages/feed/index.js
+++ P3/restify-front/src/pages/feed/index.js
@@ -10,13 +10,14 @@ const Feed = () => {
     const [count, setCount] = useState("");
     const [next, setNext] = useState("");
     const [previous, setPrevious] = useState("");
-    const [results, setResults] = useState("");
+    const [results, setResults] = useState([]);
 
     const logout = () => {
         window.localStorage.removeItem("access_token");
     }
 
     useEffect(() => {
+        console.log("hi")
         let request = get("http://127.0.0.1:8000/accounts/api/feed/", window.localStorage.getItem("access_token"))
         request.then(response => {
             if (response.status === 200) {
@@ -33,15 +34,23 @@ const Feed = () => {
 
 
     return <>
-        <div id="grid">
-        {results.map(result => (
-            <PostBox key={0} username={result.user} restaurant={result.restaurant} icon={null} 
-        timestamp={result.timestamp} text={result.body}/>
-            ))}
+        <section class="section">
+            <div class="container">
+                <div class="columns is-centered">
+                    <div class="column is-6"></div>
+                    <div id="grid">
 
-            
-        </div>
-    </>   
+                        {results.map(result => (
+                            <PostBox key={0} username={result.user} restaurant={result.restaurant} icon={null}
+                                timestamp={result.timestamp} text={result.body} />
+                        ))}
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+    </>
 }
 
 export default Feed
