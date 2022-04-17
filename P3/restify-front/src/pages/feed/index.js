f34517010301a126aa12f3f89121885721d17196
Riley Hannigan <riley.hannigan@hotmail.com>
2022-04-17 02:43:34 -0400

    edit feed

--- P3/restify-front/src/pages/feed/index.js
+++ P3/restify-front/src/pages/feed/index.js
@@ -7,6 +7,11 @@ import LikeButton from "../../components/LikeButton";
 import PostBox from "../../components/PostBox";
 
 const Feed = () => {
+    const [count, setCount] = useState("");
+    const [next, setNext] = useState("");
+    const [previous, setPrevious] = useState("");
+    const [results, setResults] = useState("");
+
     const logout = () => {
         window.localStorage.removeItem("access_token");
     }
@@ -17,6 +22,10 @@ const Feed = () => {
             if (response.status === 200) {
                 response.json().then(data => {
                     console.log(data);
+                    setCount(data.count);
+                    setNext(data.next);
+                    setPrevious(data.previous);
+                    setResults(data.results);
                 })
             }
         })
@@ -25,11 +34,12 @@ const Feed = () => {
 
     return <>
         <div id="grid">
-            <PostBox key={0} username={"user"} restaurant={"restaurant"} icon={null} 
-        timestamp = {"31 min"}text={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+        {results.map(result => (
+            <PostBox key={0} username={result.user} restaurant={result.restaurant} icon={null} 
+        timestamp={result.timestamp} text={result.body}/>
+            ))}
 
-
-    }/>
+            
         </div>
     </>   
 }
