50f3409e24ab5113f9e5fde36f84db9616d4cc53
luroy <roylu99@gmail.com>
2022-04-18 04:11:15 -0400

    add run and startup shell scripts

---
+++ P3/run.sh
@@ -0,0 +1,8 @@
+# Run server
+./manage.py runserver
+
+cd ../P3/restify-front
+
+npm install
+
+npm start
---
+++ P3/startup.sh
@@ -0,0 +1,14 @@
+echo "First Time Install"
+
+cd ../P2
+
+# Create the virtual environment and activate it
+virtualenv venv
+source venv/bin/activate
+
+# Install all requirements on the venv
+pip install -r requirements.txt
+
+# Run all migrations
+./manage.py makemigrations
+./manage.py migrate
