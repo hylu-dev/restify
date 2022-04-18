b2fa10e2f12f17aef6db6672d69c11f2881c5349
Riley Hannigan <riley.hannigan@hotmail.com>
2022-04-17 20:15:57 -0400

    add liked guard

--- P3/restify-front/src/components/LikeButton/index.js
+++ P3/restify-front/src/components/LikeButton/index.js
@@ -1,29 +1,37 @@
 import { put } from "../../utils";
 
 const LikeButton = ({ postID }) => {
+    const [liked, setLiked] = useState(false);
 
     const like_request = async e => {
-        e.preventDefault();
-        let request = put("http://127.0.0.1:8000/restaurants/api/restaurant/post/" + postID + "/like/", {}, window.localStorage.getItem("access_token"))
-        request.then(response => {
-            if (response.status === 200) {
-                response.json().then(data => {
-                    console.log(data);
-                });
-            }
-        })
+        if (!liked) {
+            e.preventDefault();
+            let request = put("http://127.0.0.1:8000/restaurants/api/restaurant/post/" + postID + "/like/", {}, window.localStorage.getItem("access_token"))
+             request.then(response => {
+                if (response.status === 200) {
+                    response.json().then(data => {
+                        console.log(data);
+                        setLiked(true);
+                    });
+                }
+            })
+        }
+        
     };
 
     const unlike_request = async e => {
-        e.preventDefault();
-        let request = put("http://127.0.0.1:8000/restaurants/api/restaurant/post/" + postID + "/unlike/", {}, window.localStorage.getItem("access_token"))
-        request.then(response => {
-            if (response.status === 200) {
-                response.json().then(data => {
-                    console.log(data);
-                });
-            }
-        })
+        if (!liked) {
+            e.preventDefault();
+            let request = put("http://127.0.0.1:8000/restaurants/api/restaurant/post/" + postID + "/unlike/", {}, window.localStorage.getItem("access_token"))
+            request.then(response => {
+                if (response.status === 200) {
+                    response.json().then(data => {
+                        setLiked(false);
+                    });
+                }
+            })
+        }
+        
     };
 
 
