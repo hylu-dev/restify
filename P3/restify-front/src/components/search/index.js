387b9e1a18960f48de74417f0c5f879e5c33ad03
luroy <roylu99@gmail.com>
2022-04-17 20:43:25 -0400

    add search

--- P3/restify-front/src/components/search/index.js
+++ P3/restify-front/src/components/search/index.js
@@ -1,10 +1,10 @@
-const Search = ({ colorClass, value, handler, styles }) => {
-    var color = colorClass ? colorClass : "is-primary"
-    var customStyles = styles ? styles : ""
+const Search = ({ }) => {
+    const [query, setQuery] = useState("");
+
     return <>
         <div className="field">
             <p className="control is-expanded has-icons-right">
-                <input className="input" type="search" placeholder="Search..." />
+                <input className="input" type="search" placeholder="Search..." onChange={e => setQuery(e.target.value)} />
                 <span className="icon is-small is-right"><i className="fas fa-search"></i></span>
             </p>
         </div>
